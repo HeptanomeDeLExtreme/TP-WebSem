8c31d85b6e6d63cdb71fb87f87714fe5ee7ac6d5
Omadz <ophelie.delsaux@hotmail.fr>
2015-11-16 11:49:49 +0100

    Enrichissement fonctionnel, il faut enlever les print

--- Test_Bonfante/enrichissement.py
+++ Test_Bonfante/enrichissement.py
@@ -1,13 +1,14 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 dictionnaire = {}
-dictionnaire["http://mcgruff.org/"] = ["http://dbpedia.org/resource/Monster_Jam","http://dbpedia.org/resource/1930s"]
+dictionnaire["http://mcgruff.org/"] = ["http://dbpedia.org/resource/Michelle_Obama"]
 
 def parcoursDict(dic):
 	dicFinal = {}
 	for cle in dic:
 		listeTemp = []
 		for value in dic[cle]:
+			listeTemp.append(value)
 			listeTemp += lanceRequete(value)
 		dicFinal[cle] = listeTemp		
 	return dicFinal
@@ -23,14 +24,23 @@ def lanceRequete(uri):
 		temp = (result["o"]["value"])
 		listeFinal.append(temp)
 	return listeFinal
-		
+
+# On trie pour recuperer les predicats qui ont la valeur
+# urlType:type
+# pour ne recuperer que des URI au niveau des objets retournes		
 def creeRequete(uri):
 	requete = """
-SELECT * 
-WHERE { ?s ?p ?o. FILTER(?s in ("""
+PREFIX urlType: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX urlSeeAlso: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT DISTINCT ?s ?o 
+WHERE { ?s ?p ?o. 
+	FILTER(?s in ("""
 	requete += '<'+uri+'>'
-	requete += """)) } 
-LIMIT 10 """
+	requete += """))
+	FILTER ( regex(?p, urlType:type) || regex(?p, urlSeeAlso:seeAlso) || regex(?p, <http://purl.org/dc/terms/subject>) )
+      } 
+LIMIT 100 """
+	print requete
 	return requete
 
 #~ print(lanceRequete("http://dbpedia.org/resource/Michelle_Obama"))
