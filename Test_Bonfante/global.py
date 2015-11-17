d0cf83223ca354b37f24169c0042ad5522a884b9
nico <nico>
2015-11-17 21:18:12 +0100

    modification du nom des fichiers

--- Test_Bonfante/global.py
+++
@@ -1,9 +0,0 @@
-def main():
-	requete = raw_input("Entrez votre requete : ")
-	print('')
-	url = "https://searx.laquadrature.net/?q=["+requete+"]&format=json"
-	html = readHTML(url)
-	dictionnairePur = JSONParser(html)
-	dictionnaireEnrichi = parcoursDict(dictionnaire) 
-
-main()
--- Test_Bonfante/main.py
+++ Test_Bonfante/main.py
@@ -1,51 +1,13 @@
-import urllib
-import json
-import spotlight
-from SPARQLWrapper import SPARQLWrapper, JSON
+from htmlReader import *
+from enrichissement import *
+from jaccard import *
 
-def readHTML(url):
-	sock = urllib.urlopen(url)
-	htmlSource = sock.read()
-	sock.close()
-	return htmlSource
+def main():
+	requete = raw_input("Entrez votre requete : ")
+	print('')
+	url = "https://searx.laquadrature.net/?q=["+requete+"]&format=json"
+	html = readHTML(url)
+	dictionnairePur = JSONParser(html)
+	dictionnaireEnrichi = parcoursDict(dictionnaire) 
 
-def find_nth(haystack, needle, n):
-    start = haystack.find(needle)
-    while start >= 0 and n > 1:
-        start = haystack.find(needle, start+len(needle))
-        n -= 1
-    return start
-
-def processHTML(htmlContent):
-	secondIndex= find_nth(htmlContent,'[',2)
-	return htmlContent[(secondIndex+1):(len(htmlContent)-2)]
-
-def JSONParser(htmlSource):
-	dict = {}
-	nbError = 0
-	parsed_json = json.loads(htmlSource)
-	for i in range(len(parsed_json['results'])):
-	#for i in range(3):
-		#print(parsed_json['results'][i]['url'])
-		try:
-			listeURI = annotateHTML(parsed_json['results'][i]['content'])
-			dict[parsed_json['results'][i]['url']] = listeURI	
-			#print(dict[parsed_json['results'][i]['url']])
-		except:
-			nbError = nbError+1
-			#print('\tERROR\n')
-	#print("Nombre d'erreurs : "+str(nbError))
-	return dict
-
-def annotateHTML(html):
-	annotation = spotlight.annotate("http://spotlight.dbpedia.org/rest/annotate",html)
-	listeURI = []
-	for i in range(len(annotation)):
-		listeURI += [annotation[i]['URI']]
-	return listeURI
-
-requete = raw_input("Entrez votre requete : ")
-print('')
-url = "https://searx.laquadrature.net/?q=["+requete+"]&format=json"
-html = readHTML(url)
-print JSONParser(html)
+main()
