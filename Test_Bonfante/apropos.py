d7efc369cc3efda1f549ca7b07b6fb8c14042f94
marion chalumeau <marion@DESKTOP-CH4NV5U>
2015-11-24 15:18:48 +0100

    apropos correction

--- Test_Bonfante/apropos.py
+++ Test_Bonfante/apropos.py
@@ -1,7 +1,6 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 def lancerRequetePays(motcle):
-	motcle.replace(' ', '_')
 	requete = genererrequete(motcle)
 	sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 	sparql.setQuery(requete)
@@ -12,28 +11,23 @@ def lancerRequetePays(motcle):
 		predicat = result["p"]["value"]
 		objet = result["o"]["value"]
 		dicFinal[predicat] = objet
-                ##### Test
-                #print "predicat : " + predicat
-                #print "objet : " + objet
-                ##### Fin Test
 	return dicFinal
 
-def genererrequete(motcle):        
+def genererrequete(motcle):
 	requete = """
 	SELECT ?p ?o
 	WHERE { ?s ?p ?o. 
         ?s rdf:type <http://schema.org/Country>.
 	FILTER(?s in (<http://dbpedia.org/resource/"""
-	requete += motcle.replace(' ', '_')
+	requete += motcle.title().replace(' ', '_')
 	requete += """>))
         FILTER ((regex(?p, rdfs:label) && langMatches(lang(?o), "en"))  || 
 	(regex(?p, rdfs:comment) && langMatches(lang(?o), "en")) || 
 	regex(?p, <http://dbpedia.org/ontology/PopulatedPlace/areaTotal>) || 
 	regex(?p, <http://xmlns.com/foaf/0.1/depiction>) || 
-	regex(?p, <http://dbpedia.org/ontology/areaTotal>) || 
-	regex(?p, <http://dbpedia.org/property/areaKm>) || 
 	regex(?p, <http://dbpedia.org/property/englishmotto>) || 
-	(regex(?p, <http://dbpedia.org/property/languages>) && langMatches(lang(?o), "en"))
-	)} 
-	LIMIT 100"""
+	(regex(?p, <http://dbpedia.org/property/languages>) && langMatches(lang(?o), "en")) ||
+        regex(?p, <http://dbpedia.org/property/populationEstimate>) ||
+        regex(?p, <http://dbpedia.org/property/capital>)
+	)} """
 	return requete
